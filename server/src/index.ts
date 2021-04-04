fb65749af397b7867dc7ea85e2465b4c49704ab9
RopFoo <ropfoo@gmail.com>
2021-04-04 08:30:40 +0200

    initialized apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,19 @@
+import 'reflect-metadata';
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import { ApolloServer } from 'apollo-server-express';
+import { resolvers } from './type-graphql';
+import { buildSchemaSync } from 'type-graphql';
+import { PrismaClient } from '.prisma/client';
 
 const port = 8080;
 const app = express();
+const prisma = new PrismaClient();
+
+const server = new ApolloServer({
+  schema: buildSchemaSync({ resolvers }),
+  context: () => ({ prisma }),
+});
+server.applyMiddleware({ app });
 
 app.get('/', (req, res) => {
   res.send('Hello tonemato!');
