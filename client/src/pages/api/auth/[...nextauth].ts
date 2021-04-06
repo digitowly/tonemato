a664da0f09164c41b24df6a460e1c311342a1efc
RopFoo <ropfoo@gmail.com>
2021-04-06 18:37:10 +0200

    added auth token + server registration

--- client/src/pages/api/auth/[...nextauth].ts
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import axios from 'axios';
 
 const options = {
   providers: [
@@ -12,7 +13,43 @@ const options = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  debug: false,
+  session: {
+    jwt: true,
+  },
+  callbacks: {
+    session: async (session, user) => {
+      session.jwt = user.jwt;
+      session.id = user.id;
+      return Promise.resolve(session);
+    },
+    jwt: async (token, user, account) => {
+      const isSignIn = user ? true : false;
+      // TODO: check if user exists
+      if (isSignIn) {
+        console.log('USER', user);
+        const response = await axios({
+          url: process.env.GRAPHQL_API_DOCKER,
+          method: 'post',
+          data: {
+            query: `
+                mutation CreateUser {
+                    createUser(data: { name: "${user.name}", email: "${user.email}" }) {
+                        id
+                        name
+                        email
+                      }
+                  }
+                `,
+          },
+        });
+
+        const data = await response.data;
+        token.jwt = data.jwt;
+        token.id = data.id;
+      }
+      return Promise.resolve(token);
+    },
+  },
 };
 
 export default (req, res) => NextAuth(req, res, options);
