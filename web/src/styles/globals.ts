38164a5adc11b40dab76a5f5bd0814bb668a8b1a
robert.pasdziernik <robert.pasdziernik@dotfly.de>
2021-08-17 08:09:26 +0200

    added heights

--- web/src/styles/globals.ts
+++ web/src/styles/globals.ts
@@ -1,8 +1,13 @@
 export const colors = Object.freeze({
-  highlight: '#5855C7',
-  primary: '#55c79e',
-  lighgrey: '#f8f8f8',
-  white: 'white',
   clash: '#55c79e',
   who: '#5855c7',
+  primary: '#55c79e',
+  highlight: '#5855C7',
+  lightgrey: '#f8f8f8',
+  grey: '#cecece',
+  white: 'white',
+});
+
+export const heights = Object.freeze({
+  formField: 55,
 });
